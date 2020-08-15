a7e89a636fecd9f8100bd53c52bafd606b227049
AlexNeep <Contact@language-mate.com>
2020-08-15 13:21:07 +0100

    description scraper & classifier complete

--- scraper.js
+++ scraper.js
@@ -1,17 +1,24 @@
 const puppeteer = require("puppeteer");
 
 require("./city_list");
-require("./functions");
 const fsLibrary = require("fs");
-const { count } = require("console");
 
 const baseURL = "https://www.lonelyplanet.com";
 const transport_end_url = "transportation";
-const city_list = defineCityTypes();
 
-async function scrapeLocation(country, city, baseURL) {
+const file = "descriptions_all.txt";
+
+appendToFile = (file, data) => {
+  fsLibrary.appendFile(file, data, (error) => {
+    if (error) {
+      console.log(error);
+      throw error;
+    }
+  });
+};
+async function scrapeLocation(country, city, baseURL, browser) {
   let url = `${baseURL}/${country}/${city}`;
-  const browser = await puppeteer.launch();
+
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
   await page.goto(url);
@@ -19,12 +26,6 @@ async function scrapeLocation(country, city, baseURL) {
   const [error_page] = await page.$x('//*[@id="__next"]/div/div/h1');
   if (!error_page) {
     console.log("success for: ", country, city);
-
-    let first_text = "";
-    let second_text = "";
-
-    let first_rawText = "";
-    let second_rawText = "";
 
     const button =
       "#introduction > div.jsx-3380108488.introduction.mx-auto > p > button";
@@ -42,40 +43,23 @@ async function scrapeLocation(country, city, baseURL) {
       article_text = await article_text.jsonValue();
 
       //   append the text and details to the description file
-      fsLibrary.appendFile(
-        "descriptions.txt",
-        `${country},${city}\n` + article_text + "\n",
-        (error) => {
-          if (error) {
-            console.log(error, country, city);
-            throw err;
-          }
-        }
-      );
+      await appendToFile(file, `${country},${city}\n` + article_text + "\n");
     } catch {
       console.log("no button");
       try {
         let [desc] = await page.$x('//*[@id="introduction"]/div[2]/div');
         let desc_text = await desc.getProperty("textContent");
         desc_text = await desc_text.jsonValue();
-        fsLibrary.appendFile(
-          "descriptions.txt",
-          `${country},${city}\n` + desc_text + "\n",
-          (error) => {
-            if (error) {
-              console.log(error, country, city);
-              throw err;
-            }
-          }
-        );
+        await appendToFile(file, `${country},${city}\n` + desc_text + "\n");
       } catch {
         console.log("no paragraph");
       }
     }
   } else {
+    await appendToFile(file, `${country},${city}\n` + "No Description" + "\n");
     console.log("error for: ", country, city);
   }
-  browser.close();
+  await page.close();
 }
 formURL = (baseURL, country, city) => {
   return `${baseURL}/${country.replace(/\s/g, "-")}/${city.replace(
@@ -87,58 +71,52 @@ formURL = (baseURL, country, city) => {
 cycleThroughCities = async (city_list, baseURL) => {
   let counter = 0;
 
-  await city_list.forEach((location) => {
-    counter++;
-    // if (counter < 11) {
-    // console.log(formURL(baseURL, location.country, location.city));
-    scrapeLocation(
-      location.country.replace(/\s/g, "-"),
-      location.city.replace(/\s/g, "-"),
-      baseURL
-    ).catch((error) => {
-      console.log(location.country, location.city);
-      console.log(error);
-    });
-    // }
-  });
+  const browser = await puppeteer.launch();
+
+  for (let counter = 0; counter < city_list.length; ) {
+    await scrapeLocation(
+      city_list[counter].country.replace(/\s/g, "-"),
+      city_list[counter].city.replace(/\s/g, "-"),
+      baseURL,
+      browser
+    )
+      .then(() => counter++)
+      .catch((error) => {
+        console.log(city_list[counter].country, city_list[counter].city);
+        console.log(error);
+      });
+  }
 };
 
-cycleThroughCities(city_list, baseURL);
-// scrapeLocation("spain", "madrid", baseURL);
-// //////////////////////// PLAN
+async function completedCities() {
+  const fs = require("fs");
+  const readline = require("readline");
+  let completed_cities = [];
+  let desc_line = false; //first line is country and city combo
 
-// url https://www.lonelyplanet.com/italy/rome/transportation
-// location of airport //*[@id="main"]/div/div[1]/div/div[1]/div[22]/article/div/div/div/div/p/span
-// //*[@id="main"]/div/div[1]/div/div[1]/div[22]/article
+  const fileStream = fs.createReadStream(file);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
 
-/*
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    if (!desc_line) {
+      let temp = line.split(",");
+      completed_cities.push(temp[1]);
+    }
+    desc_line = !desc_line;
+  }
+  return completed_cities;
+}
 
-seach each of these divs  : //*[@id="main"]/div/div[1]/div/div[1]/div[22]
-until this div equalt 'airport in ${city}' // location of airport //*[@id="main"]/div/div[1]/div/div[1]/div[22]/article/div/div/div/div/p/span
-*/
-
-// let locations = new locations();
-
-// ////////////////////////// cycle through countries
-
-// const city_list = defineCityTypes();
-// let counter = 0;
-// city_list.forEach((location) => {
-//   counter++;
-//   //   console.log(
-//   //     `${baseURL}/${location.country.replace(/\s/g, "-")}/${location.city.replace(
-//   //       /\s/g,
-//   //       "-"
-//   //     )}/transportation`
-//   //   );
-//   fsLibrary.appendFile(
-//     "cities.txt",
-//     `${baseURL}/${location.country.replace(/\s/g, "-")}/${location.city.replace(
-//       /\s/g,
-//       "-"
-//     )}/transportation` + "\n",
-//     (error) => {
-//       if (error) throw err;
+// completedCities(file).then((completed_cities) =>
+//   city_list.forEach((location) => {
+//     if (!completed_cities.includes(location.city.replace(/\s/g, "-"))) {
+//       console.log(location.city);
 //     }
-//   );
-// });
+//   })
+// );
+const city_list = defineUniqueCities();
+cycleThroughCities(city_list, baseURL);
